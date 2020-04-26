87653200dc4fbf6045d427e6854652c01c5bbdb5
sai srujana <tirunagarisaisrujana54@gmail.com>
2020-04-26 13:05:17 +0530

    add files

---
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -0,0 +1,103 @@
+package com.stackroute.keepnote.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.stackroute.keepnote.model.Note;
+
+
+/*This class will contain the application-context for the application. 
+ * Define the following annotations:
+ * @Configuration - Annotating a class with the @Configuration indicates that the 
+ *                  class can be used by the Spring IoC container as a source of 
+ *                  bean definitions
+ * @EnableTransactionManagement - Enables Spring's annotation-driven transaction management capability.
+ *                  
+ * */
+@Configuration
+@EnableTransactionManagement
+public class ApplicationContextConfig {
+
+	/*
+	 * Define the bean for DataSource. In our application, we are using MySQL as the
+	 * dataSource. To create the DataSource bean, we need to know: 1. Driver class
+	 * name 2. Database URL 3. UserName 4. Password
+	 */
+	@Bean
+	public DataSource dataSource() {
+	
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")
+				+"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+		dataSource.setUsername(System.getenv("MYSQL_USER"));
+		dataSource.setPassword(System.getenv("MYSQL_PASSWORD")); 
+
+//below 4 lines should be commented in oder to push into gitlab and verified by hobbes//		
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/keepNote?createDatabaseIfNotExist=true");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		
+		return dataSource;
+	}
+	
+
+/*
+        Use this configuration while submitting solution in hobbes.
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")
+				+"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+		dataSource.setUsername(System.getenv("MYSQL_USER"));
+		dataSource.setPassword(System.getenv("MYSQL_PASSWORD")); */
+
+	/*
+	 * Define the bean for SessionFactory. Hibernate SessionFactory is the factory
+	 * class through which we get sessions and perform database operations.
+	 */
+	@Bean
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+		sessionBuilder.scanPackages("com.stackroute.keepnote.model");
+		return sessionBuilder.buildSessionFactory();
+	}
+	
+	private Properties getHibernateProperties() {
+		Properties hibernateproperties = new Properties();
+		hibernateproperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		hibernateproperties.put("hibernate.hbm2ddl.auto", "update");
+		hibernateproperties.put("hibernate.show_sql", "true");
+		hibernateproperties.put("hibernate.format_sql", "true");
+		return hibernateproperties;
+	}
+
+	/*
+	 * Define the bean for Transaction Manager. HibernateTransactionManager handles
+	 * transaction in Spring. The application that uses single hibernate session
+	 * factory for database transaction has good choice to use
+	 * HibernateTransactionManager. HibernateTransactionManager can work with plain
+	 * JDBC too. HibernateTransactionManager allows bulk update and bulk insert and
+	 * ensures data integrity.
+	 */
+	
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txmanager = new HibernateTransactionManager(sessionFactory);
+		return txmanager;
+	}
+	
+	@Bean
+	public Note note() {
+		return new Note();
+	}
+}
---
+++ src/main/java/com/stackroute/keepnote/dao/NoteDAOImpl.java
@@ -0,0 +1,97 @@
+package com.stackroute.keepnote.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.stackroute.keepnote.model.Note;
+
+
+/*
+ * This class is implementing the NoteDAO interface. This class has to be annotated with @Repository
+ * annotation.
+ * @Repository - is an annotation that marks the specific class as a Data Access Object, thus 
+ * 				 clarifying it's role.
+ * @Transactional - The transactional annotation itself defines the scope of a single database 
+ * 					transaction. The database transaction happens inside the scope of a persistence 
+ * 					context.  
+ * */
+@Repository
+@Transactional
+public class NoteDAOImpl implements NoteDAO {
+
+	/*
+	 * Autowiring should be implemented for the SessionFactory.
+	 */
+	
+     @Autowired
+     private SessionFactory sessionFactory;
+     
+	public NoteDAOImpl(SessionFactory sessionFactory) {
+          this.sessionFactory=sessionFactory;
+	}
+
+	/*
+	 * Save the note in the database(note) table.
+	 */
+
+	@Override
+	public boolean saveNote(Note note) {
+	
+		sessionFactory.getCurrentSession().save(note);
+		return true;
+	}
+
+	/*
+	 * Remove the note from the database(note) table.
+	 */
+
+	@Override
+	public boolean deleteNote(int noteId) {
+		     Session session=sessionFactory.getCurrentSession();
+		      session.delete(getNoteById(noteId));  
+		      return true;
+
+	}
+
+	/*
+	 * retrieve all existing notes sorted by created Date in descending
+	 * order(showing latest note first)
+	 */
+	@Override
+	public List<Note> getAllNotes() {
+		return sessionFactory.getCurrentSession().createQuery("from Note order by note_creation_date desc").list();
+          
+	}
+
+	/*
+	 * retrieve specific note from the database(note) table
+	 */
+	@Override
+	public Note getNoteById(int noteId) {
+		return sessionFactory.getCurrentSession().get(Note.class,noteId);
+		    
+
+	}
+
+	/* Update existing note */
+
+	@Override
+	public boolean UpdateNote(Note note) {
+		Note note1=getNoteById(note.getNoteId());
+		if(note1!=null)
+		{
+			note1.setNoteTitle(note.getNoteTitle());
+			note1.setNoteContent(note.getNoteContent());
+			note1.setNoteStatus(note.getNoteStatus());
+			return true;
+		}
+        return false;
+	}
+
+}
